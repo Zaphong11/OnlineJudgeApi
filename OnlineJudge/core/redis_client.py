0d51197a7c7f7ea42484081a9d85c004e0698fac
Zaphong11 <phongnguyengia82@gmail.com>
2025-11-05 18:14:22 +0700

    delete redis

--- OnlineJudge/core/redis_client.py
+++
@@ -1,6 +0,0 @@
-import redis
-from core.config import settings
-
-"""Redis client setup for the Online Judge application."""
-
-redis_client = redis.Redis.from_url(settings.REDIS_URL, decode_responses=True)
